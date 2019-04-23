ee8f2e05bad9f1cc2b086266170c9ce130ef6f40
Ashish Rastogi <ashish.rastogi18@gmail.com>
2019-04-22 21:23:11 -0400

    sorted array

--- src/webparts/spFxCharts/Services/SPDataSource.ts
+++ src/webparts/spFxCharts/Services/SPDataSource.ts
@@ -52,55 +52,29 @@ export default class SPDataSource implements ISPDataSource {
     public  getToDosAsync(spHttpClient,siteurl):ChartJSData {
       let region = ['India','China','United States','United Kingdom'];
       let items=[];
-      let records=[0,1,2,3];
+      const chartJSData = new ChartJSData();
        spHttpClient.get(`${siteurl}/_api/web/lists/getbytitle('Incidents')/items`,
       SPHttpClient.configurations.v1).then((response)=>{
         if(response.ok)
         {
            response.json().then((data)=>{
             items = data.value;
+            let indlength = items.filter(d=>d.Country == "India").length;
+            let chinalength = items.filter(d=>d.Country == "China").length;
+            let uslength = items.filter(d=>d.Country == "United States").length;
+            let uklength = items.filter(d=>d.Country == "United Kingdom").length;
+           
+            chartJSData.dataSetLabel = "Real data set";
+            chartJSData.dataLabels = ["Group 1", "Group 2", "Group3", "Group 4"];
+            chartJSData.dataValues = [indlength, chinalength,uslength,uklength];
+            chartJSData.dataColors = [ "#FF6384", "#4BC0C0", "#FFCE56", "#82E0AA"];
+            
           });
+          return chartJSData;
         }
+        return chartJSData;
       });
-      let ind =0;
-      let ch=0;
-      let us = 0;
-      let uk = 0;
-
-      /* items.filter(function (record) {
-        
-       
-        if(record.country = "India")
-        {
-            ind ++;
-            
-        }
-        if(record.country = "China")
-        {
-             ch ++;
-            
-        }
-        if(record.country = "United States")
-        {
-             us ++;
-            
-        }
-        if(record.country = "United Kingdom")
-        {
-             uk ++;
-            
-        }
-       
-        records[1] = ind;
-        records[2] = ch;
-        records[3] = us;
-        records[4] = uk;
-      }); */
-      const chartJSData = new ChartJSData();
-      chartJSData.dataSetLabel = "Real data set";
-      chartJSData.dataLabels = ["Group 1", "Group 2", "Group3", "Group 4"];
-      chartJSData.dataValues = [45, 76,82,93];
-      chartJSData.dataColors = [ "#FF6384", "#4BC0C0", "#FFCE56", "#82E0AA"];
+     
       return chartJSData;
     }
  
